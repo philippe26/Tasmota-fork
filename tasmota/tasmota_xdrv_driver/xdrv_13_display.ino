1c6fb72d61ba426896cc04c1eab0441d5a52a2a9
philippe <pgrossi@free.fr>
2026-05-20 20:34:00 +0200

    merge step 7/7: display, user_config, version - complete merge

--- tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
@@ -186,6 +186,10 @@ void (* const DisplayCommand[])(void) PROGMEM = {
 
 #ifdef USE_GRAPH
 
+#ifndef NUM_GRAPHS
+#define NUM_GRAPHS      4                    // Max 16
+#endif
+
 typedef union {
   uint8_t data;
   struct {
